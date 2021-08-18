5dd9973d2cc6ac2fe9a27b1042710797476c8a28
Ilja <workgarbuzIlja@gmail.com>
2021-08-18 22:46:34 +0300

    initial

---
+++ TestXamarinApp/TestXamarinApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TestXamarinApp.ViewModels;
+using Xamarin.Forms;
+
+namespace TestXamarinApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
